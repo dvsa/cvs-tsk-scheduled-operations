8d7077184d923f192e69d220dedf02fc6bb8e202
Robert Waring <Robert.Waring@bjss.com>
2021-06-08 14:07:17 +0100

    feat(cvsb-19410): Change the last updated time by a ms to see if that triggers an update event in dynamo

--- src/services/BulkUpdateService.ts
+++ src/services/BulkUpdateService.ts
@@ -1,4 +1,7 @@
-import {DynamoDB} from 'aws-sdk'
+import {AWSError, DynamoDB} from 'aws-sdk'
+import { AttributeValue } from 'aws-sdk/clients/dynamodb'
+import { PromiseResult } from 'aws-sdk/lib/request'
+import { tr } from 'date-fns/locale'
 
 class BulkUpdateService {
     dynamo: DynamoDB
@@ -6,10 +9,38 @@ class BulkUpdateService {
         this.dynamo = dynamo
     }
 
+    private parseDate(dateString:string):Date {
+        const [date, timeWithZone] = dateString.split('T')
+        const [year, month, day] = date.split('-')
+        const time = timeWithZone.slice(0, -1)
+        const [hour, minute, secondAndMs] = time.split(':')
+        const [second, ms] = secondAndMs.split('.')
+
+        return new Date(
+            parseInt(year, 10),
+            parseInt(month, 10) - 1,
+            parseInt(day, 10),
+            parseInt(hour, 10),
+            parseInt(minute, 10),
+            parseInt(second, 10),
+            parseInt(ms, 10)
+        )
+    }
+
+    private addAMillisecond(date:Date):Date {
+        date.setMilliseconds(date.getMilliseconds() + 1);
+
+        return date;
+    }
+
+    private formatDate(date:Date):string {
+        return date.toISOString()
+    }
+
     // tablename is `cvs-${process.env.BRANCH}-(technical-records|test-results)`
     public async scanAll(tableName:string) {
         // Do a scan to get all the items
-        let fullResults:DynamoDB.ItemList = [];
+        const fullResults:DynamoDB.ItemList = [];
         let cont = true
         let exclusiveStartKey;
         let results;
@@ -19,9 +50,33 @@ class BulkUpdateService {
 
             if(results.Items) {
                 console.log(`results items length: ${results.Items.length}`)
-                const firstItem = results.Items[0]
-                console.log(firstItem)
-                fullResults = fullResults.concat(results.Items)
+                fullResults.concat(results.Items)
+
+                const promises = results.Items.map((item):Promise<string|PromiseResult<DynamoDB.UpdateItemOutput, AWSError>> => {
+                    if (item.techRecord && item.techRecord.L && item.techRecord.L?.length > 0 && item.techRecord.L[0].M) {
+                        const date = this.parseDate(item.techRecord.L[0].M.lastUpdatedAt.toString())
+                        const updatedDate = this.addAMillisecond(date)
+                        item.techRecord.L[0].M.lastUpdatedAt = this.formatDate(updatedDate) as AttributeValue
+
+                        const key = {
+                            systemNumber: item.systemNumber.S as AttributeValue,
+                            vin: item.vin.S as AttributeValue
+                        }
+
+                        return this.dynamo.updateItem(
+                            {
+                                TableName: tableName,
+                                Key:key,
+                                UpdateExpression:'SET techRecord = :tr',
+                                ExpressionAttributeValues: {':tr': item.techRecord}
+                            }
+                        ).promise()
+                    }
+
+                    return Promise.resolve('')
+                })
+
+                await Promise.all(promises)
             }
 
             if(results.LastEvaluatedKey) {
--- tests/unit/BulkUpdateService.unitTest.ts
+++ tests/unit/BulkUpdateService.unitTest.ts
@@ -15,35 +15,121 @@ describe('Bulk Update Service', () => {
         expect(results).toEqual([])
     });
 
-    it("returns the expected items for a short list", async () => {
+    it("updates items as expected", async () => {
+        const updateItem = jest.fn().mockReturnValue({
+            promise: jest.fn().mockResolvedValue('success')
+        })
         const dynamo = {
             scan: jest.fn().mockReturnValue({
-                promise: jest.fn().mockResolvedValue({Items: ['a', 'b', 'c']})
-            })
+                promise: jest.fn().mockResolvedValue(
+                    {
+                        Items: [
+                            {
+                                systemNumber: {S: '1234'},
+                                vin: {S: 'abcd'},
+                                techRecord: {
+                                    L: [
+                                        {
+                                            M: {
+                                                lastUpdatedAt: '2020-01-01T13:00:01.123Z'
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        ]
+                    }
+                )
+            }),
+            updateItem
         } as unknown as DynamoDB
         const bulkUpdate = new BulkUpdateService(dynamo)
 
-        const results = await bulkUpdate.scanAll('local');
+        await bulkUpdate.scanAll('local');
 
-        expect(results).toEqual(['a', 'b', 'c'])
+        const updateItemFirstArgument = updateItem.mock.calls[0][0]
+        const techRecord = updateItemFirstArgument.ExpressionAttributeValues[':tr']
+
+        expect(updateItem).toBeCalledTimes(1)
+        expect(techRecord.L[0].M.lastUpdatedAt).toEqual('2020-01-01T13:00:01.124Z')
     });
 
     it("returns the expected items for a paged list", async () => {
+        const updateItem = jest.fn().mockReturnValue({
+            promise: jest.fn().mockResolvedValue('success')
+        })
         const dynamo = {
             scan: jest.fn().mockReturnValueOnce({
-                promise: jest.fn().mockResolvedValue({LastEvaluatedKey: 'key1', Items: ['a', 'b', 'c']})
+                promise: jest.fn().mockResolvedValue(
+                    {
+                        LastEvaluatedKey: 'key1',
+                        Items: [
+                            {
+                                systemNumber: {S: '1234'},
+                                vin: {S: 'abcd'},
+                                techRecord: {
+                                    L: [
+                                        {
+                                            M: {
+                                                lastUpdatedAt: '2020-01-01T13:00:01.123Z'
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        ]
+                    }
+                )
             })
             .mockReturnValueOnce({
-                promise: jest.fn().mockResolvedValue({LastEvaluatedKey: 'key2', Items: ['d', 'e', 'f']})
+                promise: jest.fn().mockResolvedValue(
+                    {
+                        LastEvaluatedKey: 'key2',
+                        Items: [
+                            {
+                                systemNumber: {S: '5678'},
+                                vin: {S: 'efgh'},
+                                techRecord: {
+                                    L: [
+                                        {
+                                            M: {
+                                                lastUpdatedAt: '2020-01-01T13:00:01.154Z'
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        ]
+                    }
+                )
             })
             .mockReturnValueOnce({
-                promise: jest.fn().mockResolvedValue({Items: ['g', 'h', 'i']})
-            })
+                promise: jest.fn().mockResolvedValue(
+                    {
+                        Items: [
+                            {
+                                systemNumber: {S: '9101'},
+                                vin: {S: 'ijkl'},
+                                techRecord: {
+                                    L: [
+                                        {
+                                            M: {
+                                                lastUpdatedAt: '2020-01-01T13:00:01.167Z'
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        ]
+                    }
+                )
+            }),
+            updateItem
         } as unknown as DynamoDB
         const bulkUpdate = new BulkUpdateService(dynamo)
 
-        const results = await bulkUpdate.scanAll('local');
+        await bulkUpdate.scanAll('local');
 
-        expect(results).toEqual(['a', 'b', 'c', 'd', 'e', 'f','g', 'h', 'i'])
+        expect(updateItem).toBeCalledTimes(3)
     });
 })
